1e957056cea0502c51e18488ea39b3e476af79f9
Freek Paans <freekpaans@gmail.com>
2020-07-06 08:44:24 +0200

    alias for linkptr

--- course05/homework02/freekpaans/linked_list.cc
+++ course05/homework02/freekpaans/linked_list.cc
@@ -8,7 +8,7 @@ LinkedList::LinkedList(const LinkedList& from)
 	std::accumulate(
 		from.begin(), from.end(),
 		std::ref(mFront),
-		[](std::unique_ptr<Link>& output, const std::string& item) -> std::unique_ptr<Link>&
+		[](LinkPtr& output, const std::string& item) -> LinkPtr&
 		{
 			output.reset(new Link { item, {} });
 			return output->mNext;
@@ -27,9 +27,9 @@ LinkedList::size_type LinkedList::size() const
 	return std::distance(begin(), end());
 }
 
-static void push_at(std::unique_ptr<LinkedList::Link>& link, const std::string& str)
+static void push_at(LinkedList::LinkPtr& link, const std::string& str)
 {
-	std::unique_ptr<LinkedList::Link> newLink{new LinkedList::Link { str, {} }};
+	LinkedList::LinkPtr newLink{new LinkedList::Link { str, {} }};
 
 	newLink->mNext = std::move(link);
 	link = std::move(newLink);
@@ -55,10 +55,9 @@ const std::string& LinkedList::at(size_type index) const
 	return *std::next(begin(), index);
 }
 
-static std::unique_ptr<LinkedList::Link>& find_link(std::unique_ptr<LinkedList::Link>& from,
-													LinkedList::size_type index)
+static LinkedList::LinkPtr& find_link(LinkedList::LinkPtr& from, LinkedList::size_type index)
 {
-	std::unique_ptr<LinkedList::Link>* link = &from;
+	LinkedList::LinkPtr* link = &from;
 
 	while(index-- > 0)
 	{
